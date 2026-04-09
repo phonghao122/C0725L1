a857dbe54dbc6ff41e08d0a5ef57f1494429b60e
phonghao122 <phonghao122@gmail.com>
2026-04-10 00:56:30 +0700

    ss11

--- module_4/blog/src/main/java/com/example/blog/rest_controller/BlogRestController.java
+++ module_4/blog/src/main/java/com/example/blog/rest_controller/BlogRestController.java
@@ -1,7 +1,11 @@
 package com.example.blog.rest_controller;
 
 import com.example.blog.entity.Blog;
+import com.example.blog.entity.Category;
 import com.example.blog.service.IBlogService;
+import com.example.blog.service.ICategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,8 +13,12 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/blog")
+@CrossOrigin(origins = "*")
 public class BlogRestController {
+    @Autowired
     private IBlogService blogService;
+    @Autowired
+    private ICategoryService categoryService;
     @GetMapping("")
     public ResponseEntity<List<Blog>> getAllBlogs(){
         List<Blog> blogs = blogService.getBlogs();
@@ -18,9 +26,15 @@ public class BlogRestController {
     }
 
     @PostMapping("")
-    public ResponseEntity<Blog> saveBlog(Blog blog){
+    public ResponseEntity<Blog> saveBlog(@RequestBody Blog blog){
+        Category category = categoryService
+                .findById(blog.getCategory().getId());
+
+        blog.setCategory(category);
+
         blogService.saveBlog(blog);
-        return ResponseEntity.created(null).body(blog);
+
+        return new ResponseEntity<>(blog, HttpStatus.CREATED);
     }
 
     @DeleteMapping("/{id}")
